cdd00b0bd0b1946e2d60b01d3c358e0c82262d8d
Tim Lee <sang23141@gmail.com>
2024-06-12 21:10:24 +0900

    6_12_24

--- lib/views/home_screen/widgets/mainpostlist.dart
+++ lib/views/home_screen/widgets/mainpostlist.dart
@@ -10,6 +10,7 @@ import 'package:plo/model/user_model.dart';
 import 'package:plo/repository/firebase_user_repository.dart';
 import 'package:plo/views/home_screen/main_post_list_controller.dart';
 import 'package:plo/views/home_screen/main_post_list_provider.dart';
+import 'package:plo/views/postdetail_screen/postpicture.dart';
 
 final mainPostListCurrentUserProvider =
     FutureProvider.autoDispose<UserModel?>((ref) async {
@@ -55,22 +56,36 @@ class MainPostList extends ConsumerWidget {
                             onTap: () async {
                               if (posts.elementAt(index).showWarning) {
                                 final isConfirmed =
-                                    await showCupertinoDialog<bool>(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertBox(
-                                        title: "계속 하시겠습니까?",
-                                        onYesPressed: () {},
-                                        onNoPressed: () {});
-                                  },
-                                );
+                                    await AlertBox.showYesOrNoAlertDialogue(
+                                        context, "계속 하시겠습니까?");
+                                if (isConfirmed == true) {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => PostDetail(
+                                        postKey: posts[index],
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  return;
+                                }
                               }
                             },
+                            //이거 관련해서 나중에 추가하기
+                            // child: ExpandedItemWidget(
+                            //         item: items.elementAt(index),
+                            //         isFromBlockedUser: currentUser == null ? false : currentUser.blockedUsers.contains(items[index].uploaderUserUid),
+                            //       ));
                           );
                         },
+                        separatorBuilder: (context, index) => Container(),
+                        itemCount: posts.length + 1,
                       ),
           );
         },
+        error: (error, stackTrace) => Text("Unknow Error Occured"),
+        loading: () => Center(child: const CircularProgressIndicator()),
+        
       ),
     );
   }
